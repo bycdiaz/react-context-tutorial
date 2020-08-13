8b9cfc567da3a076585daa7b9fa4bbcabd57a817
Carlos Diaz <cdiaz86@gmail.com>
2020-08-13 18:00:36 -0400

    Adds movie list state. Creates Movie component.

---
+++ src/MovieList.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import Movie from "./Movie";
+
+function MovieList() {
+  const [movies, setMovies] = useState([
+    {
+      name: "Harry Potter",
+      price: "$10",
+      id: 23124
+    },
+    {
+      name: "Game of Thrones",
+      price: "$10",
+      id: 2566124
+    },
+    {
+      name: "Inception",
+      price: "$10",
+      id: 23524
+    }
+  ]);
+
+  return (
+    <div>
+      { movies.map(movie => (
+        <Movie name ={movie.name} price={movie.price} key={movie.id}/>
+      ))}
+    </div>
+  )
+}
+
+export default MovieList
